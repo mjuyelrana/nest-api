74030c5887f5d3e0b8c840c7ae61645d02779978
Md Juyel Rana <mjuyelrana@gmail.com>
2022-08-13 23:39:01 +0900

    Delete constants.ts file
    No need to put the credentials to all

--- assets/constants.ts
+++
@@ -1,6 +0,0 @@
-const dbName = "e_learning";
-const dbUser = "juyel";
-const dbPass = "juyel04232";
-const MONGO_URI = `mongodb+srv://${dbUser}:${dbPass}@atlascluster.rqnxi4t.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-const JWT_SECRET = dbPass;
-export { dbName, MONGO_URI, JWT_SECRET };
